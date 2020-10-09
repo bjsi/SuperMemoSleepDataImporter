77d98bc4cc9673be06245e60a2a3e5d52c8e196b
james <james@DESKTOP-JDJA6NK>
2020-10-09 12:20:02 +0100

    Created basic file writing test

--- src/SleepDataImporter.Tests/BinaryWriterTests.cs
+++ src/SleepDataImporter.Tests/BinaryWriterTests.cs
@@ -1,17 +1,35 @@
-﻿using System;
+﻿using SleepDataImporter.Models;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
 using Xunit;
 
 namespace SleepDataImporter.Tests
 {
     public class BinaryWriterTests
     {
-        [Fact]
-        public void WriteToSleepFile()
+
+        private readonly string TestPath = Path.Combine(Environment.CurrentDirectory, @"Fixtures/WRITE_TESTS_sleep.tim");
+
+        // TODO: Test writing multiple blocks.
+        // TODO: Where do the blocks get written to? At the end of the file?
+
+        [Theory]
+        [InlineData(new int[] { 2020, 09, 10, 10, 10, 0 }, new int[] { 2020, 09, 11, 11, 5, 0})]
+        public void WriteToSleepFile(int[] s, int[] e)
         {
-            // Write to a temporary file
-            // Read back and compare
+            File.Delete(TestPath);
+            var sleepReg = new SleepDataRegistry(TestPath);
+            var start = new DateTime(s[0], s[1], s[2], s[3], s[4], s[5]);
+            var end = new DateTime(e[0], e[1], e[2], e[3], e[4], e[5]);
+            var block = new SleepBlock(start, end);
+
+            bool ret = sleepReg.WriteSleepData(new List<SleepBlock> { block });
+            Assert.True(ret);
+
+            var read = sleepReg.ReadSleepData();
+            Assert.Equal(start, read[0].Start);
+            Assert.Equal(end, read[0].End);
         }
     }
 }
